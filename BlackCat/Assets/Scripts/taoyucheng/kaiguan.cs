97b13d8cf7315ad89c87ae987e1aad662d9b83d5
陶俞丞 <52514418+SimpleBoyNoSimple@users.noreply.github.com>
2019-09-03 09:55:49 +0800

    TYC
    修改了闸门和开关之间的代码

---
+++ BlackCat/Assets/Scripts/taoyucheng/kaiguan.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kaiguan : MonoBehaviour
+{
+    public Sprite switch_on;
+    public Sprite switch_off;
+    public bool s = true;
+    //public float ChangeSpriteInterval = 0.5f;
+    //private float totalTime = 0;
+    //private SpriteRenderer render = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //totalTime += Time.deltaTime;
+        //if (totalTime >= ChangeSpriteInterval)
+        //{
+        //    isLaserOn = !isLaserOn;
+        //    //totalTime = 0;
+        //}
+        //render.sprite = isLaserOn ? switch_off : switch_on;
+       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("test"))
+        {
+            GetComponent<SpriteRenderer>().sprite = switch_on;
+            s = false;
+        }
+    }
+}
---
+++ BlackCat/Assets/Scripts/taoyucheng/zhamen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zhamen : MonoBehaviour
+{
+    public GameObject switch_off;
+    public Sprite juchi1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (switch_off.GetComponent<kaiguan>().s == false)
+        {
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+}

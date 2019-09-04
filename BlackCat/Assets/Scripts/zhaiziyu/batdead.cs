c3251f6aec2bb2d24c59c5094a35c3830469495f
BeermanXantares <52514424+BeermanXantares@users.noreply.github.com>
2019-09-04 21:58:23 +0800

    batdead

---
+++ BlackCat/Assets/Scripts/zhaiziyu/batdead.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batdead : MonoBehaviour
+{
+    private bool isDead = false;
+    private Animator Anim = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isDead = true;
+            Anim.SetBool("Dead", isDead);
+            Anim.SetTrigger("DeadOnce");
+        }
+    }
+}

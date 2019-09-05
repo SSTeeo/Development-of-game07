2217f86798e6ebaea0f01c64f2f61b05b6f463c0
赵鹏 <52514377+SSTeeo@users.noreply.github.com>
2019-09-05 08:54:57 +0800

    物体标签及钻石粒子效果

---
+++ BlackCat/Assets/Scripts/zhaopeng/CarController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public WheelJoint2D LwheelJoint;
+    public WheelJoint2D RwheelJoint;
+    private JointMotor2D Left;
+    private JointMotor2D Right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LwheelJoint.useMotor = false;
+        RwheelJoint.useMotor = false;
+        Left = LwheelJoint.motor;
+        Right = RwheelJoint.motor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.name == "Diamond")
+        //{
+        //    Left.motorSpeed = -750;
+        //    Right.motorSpeed = -750;
+        //    LwheelJoint.motor = Left;
+        //    RwheelJoint.motor = Right;
+        //}
+
+        if (collision.gameObject.name == "BlackCat")
+        {
+            LwheelJoint.useMotor = true;
+            RwheelJoint.useMotor = true;
+        }
+    }
+}

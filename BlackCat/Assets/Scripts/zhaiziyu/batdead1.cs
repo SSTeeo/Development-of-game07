e87111b248d72581c95edc52bc62d4b96e742eb5
BeermanXantares <52514424+BeermanXantares@users.noreply.github.com>
2019-09-08 20:31:35 +0800

    绿蝙蝠和红蝙蝠

--- BlackCat/Assets/Scripts/zhaiziyu/batdead1.cs
+++ BlackCat/Assets/Scripts/zhaiziyu/batdead1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class batdead : MonoBehaviour
+public class batdead1 : MonoBehaviour
 {
     private Rigidbody2D GG;
     // Start is called before the first frame update
@@ -13,15 +13,15 @@ public class batdead : MonoBehaviour
 
     }
 
-// Update is called once per frame
-void Update()
-{
+    // Update is called once per frame
+    void Update()
+    {
 
-}
+    }
 
-private void OnTriggerEnter2D(Collider2D collision)
-{
-    gameObject.transform.parent.Find("greenbatbody").GetComponent<Rigidbody2D>().simulated = true;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.transform.parent.Find("redbatbody").GetComponent<Rigidbody2D>().simulated = true;
         //GG.GetComponent<Rigidbody2D>().AddTorque(0, 2);
     }
 

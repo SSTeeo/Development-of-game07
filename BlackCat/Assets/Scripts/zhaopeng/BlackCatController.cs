c51348a6a3ffbb76a525cb04e097f57c7d29c531
赵鹏 <52514377+SSTeeo@users.noreply.github.com>
2019-09-09 11:36:16 +0800

    关卡3.2

--- BlackCat/Assets/Scripts/zhaopeng/BlackCatController.cs
+++ BlackCat/Assets/Scripts/zhaopeng/BlackCatController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BlackCatController : MonoBehaviour
 {
-    public float Force = 35.0f;
-    public float Distance = 3.0f;
+    public float Force = 20.0f;
+    public float Distance = 2.2f;
     private float CurrentHeight;
     private Rigidbody2D BlackCat;
-    private Rigidbody2D Car;
     private Animator Anim;
     private bool CatJump = false;
+    private bool isDead = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,12 +30,20 @@ public class BlackCatController : MonoBehaviour
         {
             CatJump = false;
         }
-        print("CatJump:" + CatJump);
-        print("Input:" + Input.GetButton("Fire1"));
+
         if (CatJump && Input.GetButton("Fire1"))
         {
-            BlackCat.AddForce(new Vector2(5, Force));
+            BlackCat.AddForce(new Vector2(2.0f, Force));
             Anim.SetBool("Grounded", false);
+        }
+
+        if (isDead)
+        {
+            CircleCollider2D cir = BlackCat.GetComponent<CircleCollider2D>();
+            BoxCollider2D box = BlackCat.GetComponentInChildren<BoxCollider2D>();
+            cir.enabled = false;
+            box.enabled = false;
+            BlackCat.AddForce(new Vector2(-15.0f * Time.deltaTime, 15.0f * Time.deltaTime));
         }
     }
 
@@ -49,6 +57,7 @@ public class BlackCatController : MonoBehaviour
             Anim.SetBool("Grounded", true);
             Car.constraints = RigidbodyConstraints2D.None;
         }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -56,6 +65,24 @@ public class BlackCatController : MonoBehaviour
         if (collision.gameObject.CompareTag("Diamond"))
         {
             Destroy(collision.gameObject);
+        }
+
+        //if (collision.gameObject.CompareTag("Diamond"))
+        //{
+        //    isDead = true;
+        //    Anim.SetTrigger("Dead");
+        //    //Collider2D[] col = BlackCat.GetComponents<Collider2D>();
+        //    //foreach (Collider2D c in col)
+        //    //{
+        //    //    c.enabled = false;
+        //    //}
+        //    //BlackCat.transform.Translate(new Vector2(-1.0f, 1.0f));
+        //}
+
+        if (collision.gameObject.CompareTag("Fire")|| collision.gameObject.CompareTag("Bat"))
+        {
+            isDead = true;
+            Anim.SetTrigger("Dead");
         }
     }
 }

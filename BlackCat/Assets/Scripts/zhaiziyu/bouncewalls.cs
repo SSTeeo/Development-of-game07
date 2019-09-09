902bf9aeb39a154fff002b56e4ad3972c9aecb87
陶俞丞 <52514418+SimpleBoyNoSimple@users.noreply.github.com>
2019-09-09 15:46:25 +0800

    TYC
    改善场景

---
+++ BlackCat/Assets/Scripts/zhaiziyu/bouncewalls.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boucewalls : MonoBehaviour
+{
+    public bool Awesome;
+    public Vector2 direction;
+    private Transform Cat_Transform;
+    private Rigidbody2D rig;
+    private SpriteRenderer c;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cat_Transform = GameObject.FindGameObjectWithTag("BlackCat").GetComponent<Transform>();
+        rig = Cat_Transform.GetComponent<Rigidbody2D>();
+        c = Cat_Transform.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("BlackCat"))
+        {
+            ReverseMove();
+            c.flipX = false;
+        }
+    }
+
+    public void ReverseMove()
+    {
+        if (Awesome == true)
+        {
+            rig.velocity = direction;
+
+
+        }
+        else if (Awesome == false && Input.GetButton("Fire1"))
+        {
+            rig.velocity = direction;
+        }
+    }
+}

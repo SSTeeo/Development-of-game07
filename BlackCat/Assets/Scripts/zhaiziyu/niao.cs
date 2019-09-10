07fc16ff04fe0dd86a978ba511ba6b77f2d966a0
陶俞丞 <52514418+SimpleBoyNoSimple@users.noreply.github.com>
2019-09-10 11:55:22 +0800

    TYC
    修改火箭效果

--- BlackCat/Assets/Scripts/zhaiziyu/niao.cs
+++ BlackCat/Assets/Scripts/zhaiziyu/niao.cs
@@ -2,26 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class baozha : MonoBehaviour
+public class niao : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("baozha").GetComponent<ParticleSystem>().Stop();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("birdboss"))
+        if(collision.gameObject.CompareTag("Missile"))
         {
-            GameObject.Find("baozha").GetComponent<ParticleSystem>().Play();
-            
+            Destroy(collision.gameObject);
         }
     }
-    
 }

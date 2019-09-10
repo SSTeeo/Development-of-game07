810909905d0e0c77eef5fc59490ea2ec3a560c6f
BeermanXantares <52514424+BeermanXantares@users.noreply.github.com>
2019-09-10 09:20:54 +0800

    boss

--- BlackCat/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ BlackCat/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Pathfinding {
 	/// <summary>
@@ -17,22 +18,28 @@ namespace Pathfinding {
 		/// <summary>The object that the AI should move to</summary>
 		public Transform target;
 		IAstarAI ai;
+        void OnEnable()
+        {
+            ai = GetComponent<IAstarAI>();
+            // Update the destination right before searching for a path as well.
+            // This is enough in theory, but this script will also update the destination every
+            // frame as the destination is used for debugging and may be used for other things by other
+            // scripts as well. So it makes sense that it is up to date every frame.
+            if (ai != null) ai.onSearchPath += Update;
+        }
 
-		void OnEnable () {
-			ai = GetComponent<IAstarAI>();
-			// Update the destination right before searching for a path as well.
-			// This is enough in theory, but this script will also update the destination every
-			// frame as the destination is used for debugging and may be used for other things by other
-			// scripts as well. So it makes sense that it is up to date every frame.
-			if (ai != null) ai.onSearchPath += Update;
-		}
-
-		void OnDisable () {
+        void OnDisable () {
 			if (ai != null) ai.onSearchPath -= Update;
 		}
-
-		/// <summary>Updates the AI's destination every frame</summary>
-		void Update () {
+        //private void OnTriggerEnter2D(Collider2D collision)
+        //{
+        //    if (collision.gameObject.CompareTag("BlackCat"))
+        //    {
+        //        OnDisable();
+        //    }
+        //}
+        /// <summary>Updates the AI's destination every frame</summary>
+        void Update () {
 			if (target != null && ai != null) ai.destination = target.position;
 		}
 	}
